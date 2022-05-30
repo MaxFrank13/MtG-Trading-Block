284b8447d935dfe61502a25bcf9bf5de565753e2
Max <maxfrank13@gmail.com>
2022-05-29 22:17:08 -0400

    dependencies and message interface

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,17 @@
 import logo from './logo.svg';
 import './App.css';
+import MessageInterface from './messages/MessageInterface';
+
+import io from 'socket.io-client';
+
+// socket connection
+const socket = io.connect('http://localhost:3000');
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <MessageInterface
+      socket={socket}
+    />
   );
 }
 
