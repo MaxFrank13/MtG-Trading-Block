42a1b0eba30a2fd89d74d736626fc5422a29556b
Max <maxfrank13@gmail.com>
2022-06-01 18:54:28 -0400

    rearranged socket code

--- client/src/App.js
+++ client/src/App.js
@@ -23,14 +23,6 @@ import MessageInterface from './messages/MessageInterface';
 import Collection from './pages/Collection';
 import Login from './pages/Login';
 
-// Socket.io client side
-import io from 'socket.io-client';
-
-const socket = io.connect('http://localhost:3001');
-socket.on("connect_error", (err) => {
-  console.log(`connect_error due to ${err.message}`);
-});
-
 // Construct our main GraphQL API endpoint
 const httpLink = createHttpLink({
   uri: '/graphql',
@@ -88,9 +80,7 @@ function App() {
         </div>
       </Router>
       {chat && (
-        <MessageInterface
-          socket={socket}
-        />
+        <MessageInterface />
       )}
       <button
         onClick={() => setChat(!chat)}
